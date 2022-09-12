1b992abe00d113c5dae768b110307c477ec1b0bf
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-09-12 16:24:09 +0200

    Update ProcessCount.ps1
    fixed some bugs

--- ProcessCount.ps1
+++ ProcessCount.ps1
@@ -7,16 +7,18 @@ $bootuptime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
 $CurrentDate = Get-Date
 $uptime = $CurrentDate - $bootuptime
 $minutes_up = $uptime.Minutes
+$hours_up = $uptime.Hours
+echo "$hours_up"
 
 # user specific desktop folder
-$DesktopPath = [Environment]::GetFolderPath("Desktop") + "\ProcessCountLog.txt"
+$DesktopPath = "C:\Users\dunca\Desktop\ProcessCountLog.txt"
+# $DesktopPath = [Environment]::GetFolderPath("Desktop") + "\ProcessCountLog.txt"
 
 # one time use of clear content after reboot
-if ($minutes_up -cle 2){
+if ($minutes_up -cle 1 -and $hours_up -cmatch 0){
     Clear-Content "$DesktopPath"
 }
 
-while ($true) {
 # Process Count
 $psCount = (Get-Process).Count
 $Date = Get-Date
@@ -24,21 +26,24 @@ $Date = Get-Date
 # Used RAM
 $CompObject =  Get-WmiObject -Class WIN32_OperatingSystem
 $RAM = (($CompObject.TotalVisibleMemorySize - $CompObject.FreePhysicalMemory)/1024/1024)
-$RoundRAM = [math]::Round($RAM,2)
+$RAM_R = [math]::Round($RAM,2)
 
-# Average CPU Usage
+# Ram usage in %
+$Memory = ((($CompObject.TotalVisibleMemorySize - $CompObject.FreePhysicalMemory)*100)/ $CompObject.TotalVisibleMemorySize)
+$Memory_R = [math]::Round($Memory,2)
+
+# CPU Usage
 $CpuLoad = (Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average ).Average
 
 #GPU Memory Total Use
 $GpuMemTotal = (((Get-Counter "\GPU Process Memory(*)\Local Usage").CounterSamples | where CookedValue).CookedValue | measure -sum).sum
+$GpuMemTotal_R = [math]::Round($GpuMemTotal/1MB,2)
 
 #GPU Usage  
 $GpuUseTotal = (((Get-Counter "\GPU Engine(*engtype_3D)\Utilization Percentage").CounterSamples | where CookedValue).CookedValue | measure -sum).sum
+$GpuuseTotal_R = [math]::Round($GpuUseTotal,2)
 
 # pack all data into a textfile
-echo "$Date  Processcount:  $psCount ; Used RAM: $RoundRAM GB; Average CPU Load : $CpuLoad %; GPU Usage: $([math]::Round($GpuUseTotal,2))%; GPU Memory: $([math]::Round($GpuMemTotal/1MB,2)) MB" >> $DesktopPath
-
-# execute every 5 minutes
-Start-Sleep -Seconds 300
-}
+echo "$Date  Processcount:  $psCount; Used RAM: $($RAM_R)GB & $($Memory_R)%; CPU Load: $($CpuLoad)%; GPU Load: $($GpuUseTotal_R)%; GPU Memory: $($GpuMemTotal_R)MB" >> $DesktopPath
+exit
 exit
